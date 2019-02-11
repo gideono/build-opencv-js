de6e87fc0d4b3a4f30eb5063db87182dbdfd99b9
Gideon Oduro <gideon.oduro@outlook.com>
2019-02-11 21:31:52 +0100

    created copy script

--- scripts/copy.sh
+++ scripts/copy.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+
 SCRIPT_PATH="$( cd "$(dirname "$0")" ; pwd -P )"
 
-docker run -it --name build-opencv-js build-opencv-js /bin/sh
+docker cp build-opencv-js:/app/build_js ${SCRIPT_PATH}/../
